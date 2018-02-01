bb6070adf1a83787e986108685a3cc949f7c83b0
Andy Nguyen <aln20@duke.edu>
2018-01-31 19:20:15 -0500

    added blank classes

---
+++ src/cellsociety_team07/Visualizer.java
@@ -0,0 +1,14 @@
+package cellsociety_team07;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Visualizer extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
