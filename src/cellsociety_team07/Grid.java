b378db34bd13979bb6af6e3a61d488d43054df96
Brendan Cheng <bmc41@duke.edu>
2018-02-01 16:51:17 -0500

    Creating Conflicts

--- src/cellsociety_team07/Grid.java
+++ src/cellsociety_team07/Grid.java
@@ -1,5 +1,36 @@
 package cellsociety_team07;
+import java.util.HashMap;
+import java.awt.Point;
+import java.util.ArrayList;
 
 public class Grid {
 
+	/************************** FIELDS *************************/
+	
+	HashMap<Point,Cell> grid; // maps x,y coordinates of a cell on screen to the Cell object itself
+	
+	
+	/********************** CONSTRUCTOR ************************/
+	
+	/*
+	 * @param
+	 */
+	public Grid() {
+		
+	}
+	
+	
+	/********************** METHODS ****************************/
+	
+	/*
+	 * Return ArrayList of all cells
+	 */
+	public ArrayList<Cell> getCells() {
+		ArrayList<Cell> cellList = new ArrayList<Cell>();
+		for (Point p:grid.keySet()) {
+			cellList.add(grid.get(p));
+		}
+		return cellList;
+	}
+	
 }
