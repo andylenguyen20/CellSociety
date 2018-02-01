0f81fa608d5abd8a6f98b24d3d622273396c406e
Andy Nguyen <aln20@duke.edu>
2018-01-31 20:44:50 -0500

    made changes to simulation class

--- src/cellsociety_team07/Simulation.java
+++ src/cellsociety_team07/Simulation.java
@@ -1,5 +1,69 @@
 package cellsociety_team07;
 
-public class Simulation {
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
+
+import java.io.File;
+
+public class Simulation {
+	private double mySpeed;
+	private String myTitle;
+	private Grid grid;
+	public Simulation(String fileName){
+		initializeComponents(fileName);
+		start();
+	}
+	public void start(){
+		KeyFrame frame = new KeyFrame(Duration.millis(mySpeed),
+                e-> update());
+		Timeline timeline = new Timeline(frame);
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+	}
+	
+	public void update(){
+		System.out.println("hi");
+	}
+	public String getTitle(){
+		return myTitle;
+	}
+	public void setSpeed(double speed){
+		mySpeed = speed;
+	}
+	public static void main(String[] args){
+		Simulation sim = new Simulation("xml/gol_simulation.xml");
+	}
+	private Document readInFile(String fileName){
+		Document doc = null;
+		try {
+			File fXmlFile = new File(fileName);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			doc = dBuilder.parse(fXmlFile);
+			doc.getDocumentElement().normalize();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return doc;
+	}
+	private Grid setUpGrid(Document document){
+		Element gridTag = (Element) document.getElementsByTagName("grid").item(0);
+		int width = Integer.parseInt(gridTag.getAttribute("width"));
+		int height = Integer.parseInt(gridTag.getAttribute("height"));
+		return new Grid(width, height);
+	}
+	/*
+	 * taken from https://www.mkyong.com/java/how-to-read-xml-file-in-java-dom-parser/
+	 */
+	private void initializeComponents(String fileName){
+		Document document = readInFile(fileName);
+		grid = setUpGrid(document);
+		mySpeed = Integer.parseInt(document.getElementsByTagName("sim_speed").item(0).getTextContent());
+	}
 }
