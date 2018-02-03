773ebbf2ae265dba8a5d4f1be9a9640250934ee7
DANA PARK <dp178@duke.edu>
2018-02-02 19:38:02 -0500

    NodeMaker

--- src/cellsociety_team07/NodeMaker.java
+++ src/cellsociety_team07/NodeMaker.java
@@ -8,16 +8,16 @@ import javafx.scene.shape.Rectangle;
 public class NodeMaker extends StackPane{
 	private double sceneWidth = 400;
 	private double sceneHeight = 400;
-	private int n = 10;
-	private int m = 10;
-	double gridWidth = sceneWidth / n;
-	double gridHeight = sceneHeight / m;
+	private int rows = 10;
+	private int columns = 10;
+	double gridWidth = sceneWidth / rows;
+	double gridHeight = sceneHeight / columns;
 
-	    Rectangle[][] grid = new Rectangle[n][m];
+	    Rectangle[][] grid = new Rectangle[rows][columns];
 
 	    public void gridToNodes() {
-	    		for( int i=0; i < n; i++) {
-	            for( int j=0; j < m; j++) {
+	    		for( int i=0; i < rows; i++) {
+	            for( int j=0; j < columns; j++) {
 	            		Rectangle rectangle = new Rectangle( gridWidth, gridHeight);
 		            rectangle.setStroke(Color.BLACK);
 		            rectangle.setFill(Color.LIGHTBLUE);
