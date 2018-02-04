eb97e5fa7053c7a592f75c08a7bf3f3dacddc23b
Brendan Cheng <bmc41@duke.edu>
2018-02-04 17:11:17 -0500

    Updated Segregation Cell ApplyRules method

--- src/cellsociety_team07/SegregationCell.java
+++ src/cellsociety_team07/SegregationCell.java
@@ -1,24 +1,39 @@
 package cellsociety_team07;
 
-import javafx.scene.paint.Paint;
+import javafx.scene.paint.*;
 
 public class SegregationCell extends Cell{
+	
+	private static final int EMPTY = 0;
+	private static final int RED = 1;
+	private static final int BLUE = 2;
+	private static final Paint[] colors = {Color.WHITE, Color.RED, Color.BLUE};
+	private static double satisfaction; // satisfaction threshold
 
 	public SegregationCell(int initialState) {
 		super(initialState);
+		super.setColors(colors);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void applyRules() {
-		// TODO Auto-generated method stub
+		int same = 0; // number of cells of same type surrounding this cell
+		int total = this.getNeighbors().size(); // total number of neighbors
+		for (Cell neigh:this.getNeighbors()) {
+			if (neigh.getCurrentState() == this.getCurrentState())
+				same++;
+		}
 		
+		double frac = same/total;
+		
+		if (frac < satisfaction) { // if not enough similar neighbors
+			this.move(); // move to an empty cell
+		}
 	}
-
-	@Override
-	public Paint getColors() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public void move() { // move to empty cell
+		
 	}
 
 }
