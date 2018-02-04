15f9491a627cb5f6bc3a3e1a8b00ebed94ecfbbf
Brendan Cheng <bmc41@duke.edu>
2018-02-03 19:28:12 -0500

    More refactoring

--- src/cellsociety_team07/Cell.java
+++ src/cellsociety_team07/Cell.java
@@ -17,12 +17,6 @@ public abstract class Cell extends Rectangle {
 	}
 	
 	/*
-	 * A method that allows the cell to know what rules it abides by in the CA simulation and returns a next state given these rules.
-	 */
-	public void setRules(){
-		
-	}
-	/*
 	 * A method that updates the Cell’s state to the next state.
 	 */
 	public void update(){
