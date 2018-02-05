577ab404e9959267f87ced9a83573a853a8bd840
DANA PARK <dp178@duke.edu>
2018-02-04 20:07:26 -0500

    fish cell changes

--- src/cellsociety_team07/FishCell.java
+++ src/cellsociety_team07/FishCell.java
@@ -1,12 +1,7 @@
 package cellsociety_team07;
 
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 
 public class FishCell extends WatorCell{
-	public static final int WATER = 0; // an empty cell
-	public static final int FISH = 1;
-	public static final Paint[] colors = {Color.CYAN}; // Array of Col
 	private boolean toBeMoved;
 	public static final int REPRODUCTION_CHRONON = 0;
 	
@@ -23,7 +18,7 @@ public class FishCell extends WatorCell{
 		if(toBeMoved){
 			Cell replacement = cm.getRandomEmptyCell(WATER);
 			replacement.setNextState(super.getCurrentState());
-			super.setNextState(WATER);
+			super.setNextState(0);
 			//super.getProps()[0];
 		}
 	}
@@ -34,7 +29,7 @@ public class FishCell extends WatorCell{
 		int numLiveNeighbors = 0;
 		
 		for (Cell cell:super.getNeighbors()) {
-			if (cell.getCurrentState() != WATER){
+			if (cell.getCurrentState() != this.getCurrentState()){
 				numLiveNeighbors++;
 			}
 		}
--- src/cellsociety_team07/WatorCell.java
+++ src/cellsociety_team07/WatorCell.java
@@ -4,10 +4,10 @@ import javafx.scene.paint.Paint;
 import javafx.scene.paint.Color;
 
 public abstract class WatorCell extends Cell{
-	private static final int WATER = 0;
+	protected static final int WATER = 0;
 	private static final int SHARK = 1;
 	private static final int FISH = 2;
-	private static final Paint[] colors = {Color.BLACK, Color.BLUE, Color.GREEN};
+	protected static final Paint[] colors = {Color.BLACK, Color.BLUE, Color.GREEN};
 
 	public WatorCell(int state, double[] props) {
 		super(state, props);
