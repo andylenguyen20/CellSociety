fb8a17e83bc92daa45577785946bcd93dba5ef14
Brendan Cheng <bmc41@duke.edu>
2018-02-04 22:35:00 -0500

    SharkCell

--- src/cellsociety_team07/SharkCell.java
+++ src/cellsociety_team07/SharkCell.java
@@ -1,16 +1,74 @@
 package cellsociety_team07;
 
 public class SharkCell extends WatorCell{
+	
+	private boolean toBeMoved;
+	private double energy;
+	private double reproductionCounter;
+	private double reproductionTime;
+	private boolean fishNeighbor;
+	public static final int REPRODUCTION_CHRONON = 1;
+	public static final int STARTING_ENERGY = 2;
+	
 
 	public SharkCell(int state, double[] props) {
 		super(state, props);
-		// TODO Auto-generated constructor stub
+		super.setColors(colors);
+		toBeMoved = false;
+		energy = props[STARTING_ENERGY];
+		reproductionCounter = 0;
+		reproductionTime = props[REPRODUCTION_CHRONON];
 	}
 
+	public void applyRules(CellMover cm){
+		this.applyRules();
+		if(toBeMoved){
+			super.setNextState(0);
+			Cell replacement;
+			if (fishNeighbor) {
+				replacement = cm.getRandomEmptyCell(FISH);
+			} else {
+				replacement = cm.getRandomEmptyCell(WATER);
+			}
+			replacement.setNextState(super.getCurrentState());
+		}
+	}
+	
 	@Override
 	public void applyRules() {
-		// TODO Auto-generated method stub
-		
+		// decrease energy
+		decEnergy();
+		// Increase reproductionCounter
+		incReproduction();
+		// Check neighbors for movement options
+		for (Cell cell:super.getNeighbors()) {
+			if (cell.getCurrentState() == FISH) {
+				toBeMoved = true;
+				fishNeighbor = true;
+				return;
+			} else if (cell.getCurrentState() == WATER) {
+				toBeMoved = true;
+				fishNeighbor = false;
+				return;
+			}
+				else {
+				toBeMoved = false;
+				fishNeighbor = false;
+			}
+		}
 	}
 
+	// Decrease energy every turn
+	public void decEnergy() {
+		energy--;
+	}
+	
+	public void incReproduction() {
+		reproductionCounter++;
+	}
+	
+	public boolean toBeMoved() {
+		return toBeMoved;
+	}
+	
 }
--- src/cellsociety_team07/WatorCell.java
+++ src/cellsociety_team07/WatorCell.java
@@ -5,8 +5,8 @@ import javafx.scene.paint.Color;
 
 public abstract class WatorCell extends Cell{
 	protected static final int WATER = 0;
-	private static final int SHARK = 1;
-	private static final int FISH = 2;
+	protected static final int SHARK = 1;
+	protected static final int FISH = 2;
 	protected static final Paint[] colors = {Color.BLACK, Color.BLUE, Color.GREEN};
 
 	public WatorCell(int state, double[] props) {
