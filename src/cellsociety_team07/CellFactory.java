5797421d3e39de7a20ac5e903cf71ffae46e2613
DANA PARK <dp178@duke.edu>
2018-02-10 21:25:34 -0500

    rough line graph code

--- src/cellsociety_team07/CellFactory.java
+++ src/cellsociety_team07/CellFactory.java
@@ -4,28 +4,14 @@ import java.awt.Dimension;
 import java.util.List;
 
 public class CellFactory {
-	private static Cell generateRandomizedCell(String simType){
-		switch(simType){
-		case "Fire": 
-			return new FireCell();
-		case "GameOfLife":
-			return new GameOfLifeCell();
-		case "Segregation":
-			return new SegregationCell();
-		case "Wator":
-			return new FishCell();
-		default:
-			throw new BadSimulationException("Bad simulation type while getting default cell");		
-		}
-	}
-	
+
 	public static Cell generateBlankCell(String simType){
 		switch(simType){
 		case "Fire": 
 			return new FireCell();
 		case "GameOfLife":
 			return new GameOfLifeCell();
-		case "Segregation":
+		case "SegregationCell":
 			return new SegregationCell();
 		case "Wator":
 			return new FishCell();
